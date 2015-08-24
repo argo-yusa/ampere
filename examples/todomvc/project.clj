63d4e1d06658cf65f77fcd956e6503a0de8a19de
Koji Yusa <yusa@argo.bz>
2015-08-24 21:09:20 +0900

    update libs

--- examples/todomvc/project.clj
+++ examples/todomvc/project.clj
@@ -1,14 +1,14 @@
 (defproject todomvc-ampere "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                 [org.clojure/clojurescript "0.0-3308"]
-                 [ampere "0.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.107"]
+                 [condense/ampere "0.2.0-SNAPSHOT"]
 
                  ;[org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 ;[freactive.core "0.2.0-SNAPSHOT"]
-                 [org.omcljs/om "0.8.8"]
+                 ;[condense/freactive.core "0.2.2"]
+                 [org.omcljs/om "0.9.0"]
                  [reagent "0.5.0"]
 
-                 [sablono "0.3.4"]
+                 [sablono "0.3.6"]
                  [secretary "1.2.3"]]
 
   :plugins [[lein-cljsbuild "1.0.6"]]
